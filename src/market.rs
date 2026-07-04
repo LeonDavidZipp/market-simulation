a58c9665b0af7ddfdbe9fdce3fbca4aadd941edb
Leon David Zipp <leondavidzipp@gmx.de>
2026-07-04 20:44:08 +0200

    refactor: simplified orders: removed is_buy, this now gets determined by wthetehr they re in the bid or asks attribute

--- src/market.rs
+++ src/market.rs
@@ -5,24 +5,11 @@ use std::collections::{BTreeMap, VecDeque};
 struct Order {
     price: f32,
     quantity: f32,
-    is_buy: bool,
 }
 
 impl Order {
-    pub fn new_buy(price: f32, quantity: f32) -> Order {
-        Order {
-            price,
-            quantity,
-            is_buy: true,
-        }
-    }
-
-    pub fn new_sell(price: f32, quantity: f32) -> Order {
-        Order {
-            price,
-            quantity,
-            is_buy: false,
-        }
+    pub fn new(price: f32, quantity: f32) -> Order {
+        Order { price, quantity }
     }
 }
 
@@ -32,13 +19,16 @@ struct OrderBook {
 }
 
 impl OrderBook {
-    pub fn new(orders: Vec<Order>) -> OrderBook {
+    pub fn new(bids: Vec<Order>, asks: Vec<Order>) -> OrderBook {
         let mut book = OrderBook {
             bids: BTreeMap::new(),
             asks: BTreeMap::new(),
         };
-        for order in orders {
-            book.insert(order);
+        for b in bids {
+            book.insert_bid(b);
+        }
+        for a in asks {
+            book.insert_ask(a);
         }
         book
     }
@@ -50,13 +40,16 @@ impl OrderBook {
         }
     }
 
-    fn insert(&mut self, order: Order) {
-        let side = if order.is_buy {
-            &mut self.bids
-        } else {
-            &mut self.asks
-        };
-        side.entry(OrderedFloat(order.price))
+    fn insert_bid(&mut self, order: Order) {
+        self.bids
+            .entry(OrderedFloat(order.price))
+            .or_insert_with(VecDeque::new)
+            .push_back(order);
+    }
+
+    fn insert_ask(&mut self, order: Order) {
+        self.asks
+            .entry(OrderedFloat(order.price))
             .or_insert_with(VecDeque::new)
             .push_back(order);
     }
@@ -116,32 +109,32 @@ mod tests {
 
     #[test]
     fn test_new() {
-        let orders = sample_orders();
-        let mut book = OrderBook::new(orders);
+        let (bids, asks) = sample_orders();
+        let mut book = OrderBook::new(bids, asks);
 
         assert_eq!(book.bids.len(), 3);
         let (top_price, mut top_level) = book.bids.pop_last().unwrap();
         let (low_price, mut low_level) = book.bids.pop_first().unwrap();
 
         assert_eq!(top_price, 12.0);
-        assert_eq!(top_level.pop_back(), Some(Order::new_buy(12.0, 13.0)));
+        assert_eq!(top_level.pop_back(), Some(Order::new(12.0, 13.0)));
         assert_eq!(low_price, 11.0);
-        assert_eq!(low_level.pop_front(), Some(Order::new_buy(11.0, 12.0)));
+        assert_eq!(low_level.pop_front(), Some(Order::new(11.0, 12.0)));
 
         assert_eq!(book.asks.len(), 3);
         let (top_price, mut top_level) = book.asks.pop_last().unwrap();
         let (low_price, mut low_level) = book.asks.pop_first().unwrap();
 
         assert_eq!(top_price, 12.0);
-        assert_eq!(top_level.pop_back(), Some(Order::new_sell(12.0, 13.0)));
+        assert_eq!(top_level.pop_back(), Some(Order::new(12.0, 13.0)));
         assert_eq!(low_price, 11.0);
-        assert_eq!(low_level.pop_front(), Some(Order::new_sell(11.0, 12.0)));
+        assert_eq!(low_level.pop_front(), Some(Order::new(11.0, 12.0)));
     }
 
     #[test]
     fn test_resolve_simple() {
-        let orders = simple_resolveable_orders();
-        let mut book = OrderBook::new(orders);
+        let (bids, asks) = simple_resolveable_orders();
+        let mut book = OrderBook::new(bids, asks);
         book.resolve();
         assert!(book.bids.is_empty());
         assert!(book.asks.is_empty());
@@ -149,34 +142,34 @@ mod tests {
 
     #[test]
     fn test_resolve_leftover_buy() {
-        let orders = leftover_buy();
-        let mut book = OrderBook::new(orders);
+        let (bids, asks) = leftover_buy();
+        let mut book = OrderBook::new(bids, asks);
         book.resolve();
 
         assert!(!book.bids.is_empty());
         let (top_price, mut top_level) = book.bids.pop_last().unwrap();
         assert_eq!(top_price, 11.0);
-        assert_eq!(top_level.pop_front(), Some(Order::new_buy(11.0, 12.0)));
+        assert_eq!(top_level.pop_front(), Some(Order::new(11.0, 12.0)));
         assert!(book.asks.is_empty());
     }
 
     #[test]
     fn test_resolve_leftover_sell() {
-        let orders = leftover_sell();
-        let mut book = OrderBook::new(orders);
+        let (bids, asks) = leftover_sell();
+        let mut book = OrderBook::new(bids, asks);
         book.resolve();
 
         assert!(book.bids.is_empty());
         assert!(!book.asks.is_empty());
         let (top_price, mut top_level) = book.asks.pop_last().unwrap();
         assert_eq!(top_price, 12.0);
-        assert_eq!(top_level.pop_front(), Some(Order::new_sell(12.0, 12.0)));
+        assert_eq!(top_level.pop_front(), Some(Order::new(12.0, 12.0)));
     }
 
     #[test]
     fn test_partially_resolve_full() {
-        let orders = partially_resolve_full();
-        let mut book = OrderBook::new(orders);
+        let (bids, asks) = partially_resolve_full();
+        let mut book = OrderBook::new(bids, asks);
         book.resolve();
 
         assert!(book.bids.is_empty());
@@ -185,84 +178,83 @@ mod tests {
 
     #[test]
     fn test_partially_resolve_leftover_buy() {
-        let orders = partially_resolve_leftover_buy();
-        let mut book = OrderBook::new(orders);
+        let (bids, asks) = partially_resolve_leftover_buy();
+        let mut book = OrderBook::new(bids, asks);
         book.resolve();
 
         assert!(book.asks.is_empty());
         assert!(!book.bids.is_empty());
         let (price, mut level) = book.bids.pop_last().unwrap();
         assert_eq!(price, 11.5);
-        assert_eq!(level.pop_front(), Some(Order::new_buy(11.5, 1.0)));
+        assert_eq!(level.pop_front(), Some(Order::new(11.5, 1.0)));
         assert!(level.is_empty());
     }
 
     #[test]
     fn test_partially_resolve_leftover_sell() {
-        let orders = partially_resolve_leftover_sell();
-        let mut book = OrderBook::new(orders);
+        let (bids, asks) = partially_resolve_leftover_sell();
+        let mut book = OrderBook::new(bids, asks);
         book.resolve();
 
         assert!(book.bids.is_empty());
         assert!(!book.asks.is_empty());
         let (price, mut level) = book.asks.pop_last().unwrap();
         assert_eq!(price, 11.5);
-        assert_eq!(level.pop_front(), Some(Order::new_sell(11.5, 1.0)));
+        assert_eq!(level.pop_front(), Some(Order::new(11.5, 1.0)));
         assert!(level.is_empty());
     }
 
-    fn sample_orders() -> Vec<Order> {
-        vec![
-            Order::new_buy(11.0, 12.0),
-            Order::new_buy(12.0, 13.0),
-            Order::new_buy(11.5, 130.0),
-            Order::new_sell(11.0, 12.0),
-            Order::new_sell(12.0, 13.0),
-            Order::new_sell(11.5, 130.0),
-        ]
+    fn sample_orders() -> (Vec<Order>, Vec<Order>) {
+        (
+            vec![
+                Order::new(11.0, 12.0),
+                Order::new(12.0, 13.0),
+                Order::new(11.5, 130.0),
+            ],
+            vec![
+                Order::new(11.0, 12.0),
+                Order::new(12.0, 13.0),
+                Order::new(11.5, 130.0),
+            ],
+        )
     }
 
-    fn simple_resolveable_orders() -> Vec<Order> {
-        vec![Order::new_buy(11.0, 12.0), Order::new_sell(11.0, 12.0)]
+    fn simple_resolveable_orders() -> (Vec<Order>, Vec<Order>) {
+        (vec![Order::new(11.0, 12.0)], vec![Order::new(11.0, 12.0)])
     }
 
-    fn leftover_buy() -> Vec<Order> {
-        vec![
-            Order::new_buy(11.0, 12.0),
-            Order::new_buy(12.0, 12.0),
-            Order::new_sell(11.5, 12.0),
-        ]
+    fn leftover_buy() -> (Vec<Order>, Vec<Order>) {
+        (
+            vec![Order::new(11.0, 12.0), Order::new(12.0, 12.0)],
+            vec![Order::new(11.5, 12.0)],
+        )
     }
 
-    fn leftover_sell() -> Vec<Order> {
-        vec![
-            Order::new_buy(11.5, 12.0),
-            Order::new_sell(11.0, 12.0),
-            Order::new_sell(12.0, 12.0),
-        ]
+    fn leftover_sell() -> (Vec<Order>, Vec<Order>) {
+        (
+            vec![Order::new(11.5, 12.0)],
+            vec![Order::new(11.0, 12.0), Order::new(12.0, 12.0)],
+        )
     }
 
-    fn partially_resolve_full() -> Vec<Order> {
-        vec![
-            Order::new_buy(11.5, 25.0),
-            Order::new_sell(11.0, 12.0),
-            Order::new_sell(11.5, 13.0),
-        ]
+    fn partially_resolve_full() -> (Vec<Order>, Vec<Order>) {
+        (
+            vec![Order::new(11.5, 25.0)],
+            vec![Order::new(11.0, 12.0), Order::new(11.5, 13.0)],
+        )
     }
 
-    fn partially_resolve_leftover_buy() -> Vec<Order> {
-        vec![
-            Order::new_buy(11.5, 26.0),
-            Order::new_sell(11.0, 12.0),
-            Order::new_sell(11.5, 13.0),
-        ]
+    fn partially_resolve_leftover_buy() -> (Vec<Order>, Vec<Order>) {
+        (
+            vec![Order::new(11.5, 26.0)],
+            vec![Order::new(11.0, 12.0), Order::new(11.5, 13.0)],
+        )
     }
 
-    fn partially_resolve_leftover_sell() -> Vec<Order> {
-        vec![
-            Order::new_buy(11.5, 24.0),
-            Order::new_sell(11.0, 12.0),
-            Order::new_sell(11.5, 13.0),
-        ]
+    fn partially_resolve_leftover_sell() -> (Vec<Order>, Vec<Order>) {
+        (
+            vec![Order::new(11.5, 24.0)],
+            vec![Order::new(11.0, 12.0), Order::new(11.5, 13.0)],
+        )
     }
 }
