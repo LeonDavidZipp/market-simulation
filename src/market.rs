a17d4712c862de9cb1353eb49f397a23f1b491f9
Leon David Zipp <leondavidzipp@gmx.de>
2026-07-05 12:56:24 +0200

    refactor: moved config into Market

--- src/market.rs
+++ src/market.rs
@@ -27,48 +27,74 @@ impl From<NormalError> for MarketError {
 
 #[derive(Clone)]
 pub struct Market {
-    size: usize,
+    config: MarketConfig,
     order_book: OrderBook,
     history: Vec<CandleData>,
 }
 
-pub struct RunConfig {
+#[derive(Clone, Copy)]
+pub struct MarketConfig {
+    pub market_size: usize,
     pub start_prize: f32,
+    pub start_prize_stddev: f32,
     pub n_runs: usize,
     pub min_quantity: f32,
     pub max_quantity: f32,
     pub buyer_ratio_stddev: f32,
 }
 
+impl MarketConfig {
+    pub fn new(
+        market_size: usize,
+        start_prize: f32,
+        start_prize_stddev: f32,
+        n_runs: usize,
+        min_quantity: f32,
+        max_quantity: f32,
+        buyer_ratio_stddev: f32,
+    ) -> MarketConfig {
+        MarketConfig {
+            market_size,
+            start_prize,
+            start_prize_stddev,
+            n_runs,
+            min_quantity,
+            max_quantity,
+            buyer_ratio_stddev,
+        }
+    }
+}
+
 impl Market {
-    pub fn with_size(size: usize) -> Market {
+    pub fn with_config(cfg: MarketConfig) -> Market {
         Market {
-            size,
+            config: cfg,
             order_book: OrderBook::default(),
             history: Vec::with_capacity(16),
         }
     }
 
-    pub fn run(&mut self, cfg: &RunConfig) -> Result<(), MarketError> {
+    pub fn run(&mut self) -> Result<(), MarketError> {
+        let cfg = self.config;
         let mut start_prize = cfg.start_prize;
         let buyer_ratio_dist: Normal<f32> = Normal::new(0.5, cfg.buyer_ratio_stddev)?;
         let mut rng: rand::prelude::ThreadRng = rng();
         for _ in 0..cfg.n_runs {
-            start_prize = self.run_single(cfg, start_prize, &mut rng, &buyer_ratio_dist)?;
+            start_prize = self.run_single(start_prize, &mut rng, &buyer_ratio_dist)?;
         }
         Ok(())
     }
 
     fn run_single(
         &mut self,
-        cfg: &RunConfig,
         start_prize: f32,
         rng: &mut rand::prelude::ThreadRng,
         buyer_ratio_dist: &Normal<f32>,
     ) -> Result<f32, MarketError> {
+        let cfg = self.config;
         let buyer_ratio: f32 = buyer_ratio_dist.sample(rng).clamp(0.4, 0.6);
-        let n_buyers = (self.size as f32 * buyer_ratio).round() as usize;
-        let n_sellers = self.size - n_buyers;
+        let n_buyers = (cfg.market_size as f32 * buyer_ratio).round() as usize;
+        let n_sellers = cfg.market_size - n_buyers;
 
         let buy_orders: Vec<Order> = (0..n_buyers)
             .map(|_| {
